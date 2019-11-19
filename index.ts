f42f1a43690abcc35338fde13abebfefe71137ed
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-11-19 17:26:25 +0000

    ensure this context for getters is correct

--- index.ts
+++ index.ts
@@ -28,14 +28,13 @@ function First() {
   return function(target: any, name: string) {
     const hiddenInstanceKey = "_$$" + name + "$$_";
     const prevInit = Object.getOwnPropertyDescriptor(target, name).get;
-    const init = () => {
+    const init = (prevInit: () => any) => {
       return prevInit()
         .then(response => response[0]);
     };
-
     Object.defineProperty(target, name, {
       get: function() {
-        return this[hiddenInstanceKey] || (this[hiddenInstanceKey] = init());
+        return this[hiddenInstanceKey] || (this[hiddenInstanceKey] = init(prevInit.bind(this)));
       },
       configurable: true
     });
